37ee1f72cdde3f6426f5be85cfe469d85d7ca593
Claire Castanet <clairecastanet@MASTAFF-Claire-Castanet-Q14CH4R1PW.local>
2024-03-11 17:14:44 +0000

    sorted multer + some routes on the backend, PUT request for user + updated user creation

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,25 +1,59 @@
 const User = require("../models/user");
+const { generateToken } = require("../lib/token");
 
-const create = (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const username = req.body.username;
+const create = async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    const username = req.body.username;
+    let imgString;
 
-  const user = new User({ email, password, username });
-  user
-    .save()
-    .then((user) => {
-      console.log("User created, id:", user._id.toString());
-      res.status(201).json({ message: "OK" });
-    })
-    .catch((err) => {
-      // console.error(err);
+    if (req.file) {
+      imgString = `"assets/uploads/${req.file.filename}`;
+    } else {
+      imgString = "assets/blank-profile-picture-973460_640.png"
+    }
+
+    console.log(imgString);
+    console.log(req.body)
+
+    const user = new User({ email, password, username, imgString });
+    console.log(user)
+
+    await user.save()
+    console.log("User created, id:", user._id.toString());
+    res.status(201).json({ message: "OK" });
+  
+
+  } catch (err) {
+      console.log(err);
       res.status(400).json({ message: "Something went wrong" });
-    });
+    }
 };
+
+const updateProfilePicture = async (req, res) => {
+  try {
+    const imgString = `/assets/uploads/${req.file.filename}`;
+
+    const user = await User.find({ _id: req.user_id });
+    user[0].imgString = imgString;
+
+    await User.findOneAndUpdate({ _id: req.user_id }, user[0]);
+
+    const token = generateToken(req.user_id);
+    res.status(201).json({ message: `User ${req.user_id} profile picture has been uplodad`, token: token });
+    
+  } catch(error) { 
+    console.log(error)
+    res.status(400).json({ message: "Something went wrong" });
+  }
+}
 
 const UsersController = {
   create: create,
+  updateProfilePicture: updateProfilePicture,
 };
+
+
 
 module.exports = UsersController;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,16 @@
 const express = require("express");
-
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
 const UsersController = require("../controllers/users");
+const upload = require("../middleware/multerSetup");
+const tokenChecker = require("../middleware/tokenChecker")
 
 const router = express.Router();
 
-router.post("/", UsersController.create);
+router.post("/", upload.single('profilePicture'), UsersController.create);
+router.put("/", tokenChecker, upload.single('profilePicture'), UsersController.updateProfilePicture )
+
+
+//get request to get a user info? 
 
 module.exports = router;
